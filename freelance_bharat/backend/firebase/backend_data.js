99f2821e61bb71e1d20407fe413fb55f9fe04ae5
Aadarsh <aadarsh9811594152@gmail.com>
2023-05-20 01:59:26 +0530

    feat ---added call and message and linked it to dial and message screen

--- freelance_bharat/backend/firebase/backend_data.js
+++ freelance_bharat/backend/firebase/backend_data.js
@@ -19,6 +19,7 @@ import {
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { Image } from "react-native";
+import { Linking } from "react-native";
 
 
 const Backend_Data = (props) => {
@@ -30,109 +31,120 @@ const Backend_Data = (props) => {
     const [loading, setLoading] = useState(true);
   
     const fetchUserData = async (profession) => {
-      const db = getFirestore();
-      let q = collection(db, "users");
-      if (profession) {
-        q = query(q, where("profession", "==", profession));
-      }
-      const querySnapshot = await getDocs(q);
-      const storage = getStorage();
-      const userData = await Promise.all(
-        querySnapshot.docs.map(async (doc) => {
-          const profileImageUrl = await getDownloadURL(
-            ref(storage, doc.data().imageUrl)
-          );
-          return {
-            name: doc.data().fullName,
-            profile_image: profileImageUrl,
-            address: doc.data().address,
-            profession: doc.data().profession,
-            contact: doc.data().phoneNumber,
-            id: doc.id,
-            userId: doc.data().userId,
-            ...doc.data(),
-          };
-        })
-      );
-      setData(userData);
-      setLoading(false);
-    };
-  
-    useEffect(() => {
-      fetchUserData(props.profession);
-    }, [props.profession]);
-  
-    useEffect(() => {
-      console.log('UID changed:', props.uid);
-    }, [props.uid]);
+        const db = getFirestore();
+        let q = collection(db, "users");
+        if (profession) {
+          q = query(q, where("profession", "==", profession));
+        }
+        const querySnapshot = await getDocs(q);
+        const storage = getStorage();
+        const userData = await Promise.all(
+          querySnapshot.docs.map(async (doc) => {
+            const profileImageUrl = await getDownloadURL(
+              ref(storage, doc.data().imageUrl)
+            );
+            return {
+              name: doc.data().fullName,
+              profile_image: profileImageUrl,
+              address: doc.data().address,
+              profession: doc.data().profession,
+              contact: doc.data().phoneNumber,
+              id: doc.id,
+              userId: doc.data().userId,
+              ...doc.data(),
+            };
+          })
+        );
+        setData(userData);
+        setLoading(false);
+      };
     
-
-  
-    const renderItem = ({ item }) => (
-  
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          marginBottom: 20,
-        }}
-      >
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <TouchableOpacity
-          >
-  <View style={{ overflow: "hidden", borderRadius: 50, marginLeft: 10, position: "relative" }}>
-    <Image
-      style={[{ width: 50, height: 50, borderRadius: 50 }]}
-      source={{ uri: item.profile_image }}
-    />
-    {item.userId === uid && (
-      <View style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: 50,
-        height: 50,
-        borderRadius: 50,
-        borderColor: "green",
-        borderWidth: 5
-      }}>
-      </View>
-    )}
-  </View>
-  
-  
-  
-          </TouchableOpacity>
-  
-          <TouchableOpacity>
-            <View
-              style={{
-                marginLeft: 10,
-                width: 215,
-              }}
+      useEffect(() => {
+        fetchUserData(props.profession);
+      }, [props.profession]);
+    
+      useEffect(() => {
+        console.log('UID changed:', props.uid);
+      }, [props.uid]);
+      
+    
+      const handCallClick = (phoneNumber) => {
+        Linking.openURL(`tel:${phoneNumber}`);
+      };
+    
+      const handleMessageClick = (phoneNumber) => {
+        const messageUrl = `sms:${phoneNumber}`;
+        Linking.openURL(messageUrl);
+      };
+    
+      const renderItem = ({ item }) => (
+    
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: 20,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("UserProfile", { state: item })}
             >
-              <Text style={{ fontSize: 17 }}>{item.name}</Text>
-  
-              <Text style={{ fontSize: 11, color: "grey" }}>{item.address}</Text>
-            </View>
-          </TouchableOpacity>
+    <View style={{ overflow: "hidden", borderRadius: 50, marginLeft: 10, position: "relative" }}>
+      <Image
+        style={[{ width: 50, height: 50, borderRadius: 50 }]}
+        source={{ uri: item.profile_image }}
+      />
+      {item.userId === uid && (
+        <View style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: 50,
+          height: 50,
+          borderRadius: 50,
+          borderColor: "green",
+          borderWidth: 5
+        }}>
         </View>
-        <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity
-            style={{ marginRight: 20 }}
-          >
-            <Icon name="phone" size={25} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{ marginRight: 20 }}
-                              >
-            <Icon name="comment" size={25} color="black" />
-          </TouchableOpacity>
+      )}
+    </View>
+    
+    
+    
+            </TouchableOpacity>
+    
+            <TouchableOpacity>
+              <View
+                style={{
+                  marginLeft: 10,
+                  width: 215,
+                }}
+              >
+                <Text style={{ fontSize: 17 }}>{item.name}</Text>
+    
+                <Text style={{ fontSize: 11, color: "grey" }}>{item.address}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity
+              style={{ marginRight: 20 }}
+              onPress={() => handCallClick(item.contact)}
+            >
+              <Icon name="phone" size={25} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ marginRight: 20 }}
+              onPress={() => handleMessageClick(item.contact)}
+            >
+              <Icon name="comment" size={25} color="black" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    );
-
+      );
+    
   const renderFooter = () => {
     if (loading) {
       return (
