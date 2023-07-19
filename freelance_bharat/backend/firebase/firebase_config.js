0e1aadda9fc11660a20e3375973c7d8c6e115d18
Aadarsh <aadarsh9811594152@gmail.com>
2023-07-19 18:12:04 +0530

    added secrets in dot env

--- freelance_bharat/backend/firebase/firebase_config.js
+++ freelance_bharat/backend/firebase/firebase_config.js
@@ -1,22 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { applyActionCode } from "firebase/auth";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDwIKjjsSsuLDU1WmRbevGKSfYzAenyl2Q",
-//   authDomain: "naukribharat-21927.firebaseapp.com",
-//   databaseURL: "https://naukribharat-21927-default-rtdb.firebaseio.com",
-//   projectId: "naukribharat-21927",
-//   storageBucket: "naukribharat-21927.appspot.com",
-//   messagingSenderId: "904614555920",
-//   appId: "1:904614555920:web:aff55cfb9e92a658157595",
-//   measurementId: "G-3DXNW4XW4Y",
-// };
+import EXPO_PUBLIC_API_KEY from "./../../.env";
+import EXPO_PUBLIC_MSG_ID from "./../../.env";
+import EXPO_PUBLIC_APP_ID from "./../../.env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDayI_ewGv_eg9ATmF3Hn_PLdQaisDE2kU",
-  authDomain: "mobile-app-c8db0.firebaseapp.com",
-  projectId: "mobile-app-c8db0",
-  storageBucket: "mobile-app-c8db0.appspot.com",
-  messagingSenderId: "985687342535",
-  appId: "1:985687342535:web:177ba7a8c5dbe153a3ade2",
-  measurementId: "G-31EVKWR3KM",
+  apiKey: EXPO_PUBLIC_API_KEY,
+  authDomain: "nextjs-b3689.firebaseapp.com",
+  projectId: "nextjs-b3689",
+  storageBucket: "nextjs-b3689.appspot.com",
+  messagingSenderId: EXPO_PUBLIC_MSG_ID,
+  appId: EXPO_PUBLIC_APP_ID,
 };
+
+// Initialize Firebase
 export const firebase = initializeApp(firebaseConfig);
